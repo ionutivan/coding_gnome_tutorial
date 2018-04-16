e557a977491604076251a5dba48fbdde4016c190
IvanIo <Y92z#tT!C5Y>
2018-04-16 17:43:48 +0200

    clean dictionary

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -10,14 +10,4 @@ defmodule Dictionary do
     |> String.split(~r/\n/)
   end
 
-  def len([]), do: 0
-  def len([ _head | tail]), do: 1 + len(tail)
-  def sum([]), do: 0
-  def sum([ head | tail]), do: head + sum(tail)
-  def square([]), do: []
-  def square([ head | tail ]), do: [ head*head | square(tail) ]
-  def sum_pairs([]), do: 0
-  def sum_pairs([ head1, head2 | tail ]), do: [ head1+head2 | sum_pairs(tail) ]
-  def even_length([_head1, head2 |_]), do: head2 == nil
-  
 end
