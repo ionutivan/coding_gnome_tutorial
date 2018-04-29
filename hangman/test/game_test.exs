449b0c34a36a3fc22946b94d8735a2f134207a36
ionutivan <ionut@brokoliapp.com>
2018-04-29 23:38:41 +0200

    add hangman game

---
+++ hangman/test/game_test.exs
@@ -0,0 +1,20 @@
+defmodule GameTest do
+  use ExUnit.Case
+
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "state isn't changed for :won game" do
+    game = Game.new_game()
+            |> Map.put(:game_state, :won)
+    { new_game, _ } = Game.make_move(game, "x")
+    assert new_game == game
+  end
+
+end
