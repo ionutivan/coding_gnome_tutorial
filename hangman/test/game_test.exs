b10a02332987a0ae057d77c41bdefb89634ab6df
IvanIo <Y92z#tT!C5Y>
2018-04-30 17:39:55 +0200

    finish the hangman game

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -10,11 +10,74 @@ defmodule GameTest do
     assert length(game.letters) > 0
   end
 
-  test "state isn't changed for :won game" do
-    game = Game.new_game()
-            |> Map.put(:game_state, :won)
-    { new_game, _ } = Game.make_move(game, "x")
-    assert new_game == game
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert ^game = Game.make_move(game, "x")
+    end
   end
 
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    game = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is already used" do
+    game = Game.new_game()
+    game = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    game = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "w")
+    game = Game.make_move(game, "i")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "b")
+    game = Game.make_move(game, "l")
+    game = Game.make_move(game, "e")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
+
+  test "bad guess is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "lost game is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "a")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+    game = Game.make_move(game, "p")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 5
+    game = Game.make_move(game, "c")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 4
+    game = Game.make_move(game, "d")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 3
+    game = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 2
+    game = Game.make_move(game, "y")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 1
+    game = Game.make_move(game, "s")
+    assert game.game_state == :lost
+  end
 end
